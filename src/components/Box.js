fe35ad49e4e249a37c216db3b4990588f7690106
Ayush <ayushsagarmk42@gmail.com>
2022-06-16 18:53:42 +0530

    'proto3'

--- src/components/Box.js
+++ src/components/Box.js
@@ -6,7 +6,6 @@ import img2 from './images/o.png';
 
 
 export const Box = ({ value, onClick }) => {
-    // const style = value === " " ? "box x" : "box o";
     const src = value === " " ? img1 : img2;
 
     if (value === null) {
@@ -14,29 +13,6 @@ export const Box = ({ value, onClick }) => {
     }
 
     return (
-        // <button className={style} onClick={onClick}>{value}</button>
         <img className="box" src={src} />
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export const Box = () => {
-//   return (
-//     <button className="box"><img className="imagex" src= {img1} height={100} width={100}/></button>
-//     // <div className="App"><img src= {img1} />
-//     // </div>
-//   )
-// }
